b3ca50be8c8996d986f191ba5cc72eaa7d2733d1
Ludwig-lf <ludvig.heck@elev.ga.ntig.se>
2024-08-28 14:37:19 +0200

    fixat så att bokning knappen visas när man har tryckt på en av tiderna

--- pages/bokning_sida.js
+++ pages/bokning_sida.js
@@ -4,6 +4,7 @@ const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
 const clickableDate = document.getElementById('day');
 const checkclick = document.getElementById('D');
+const showbuttonclick = document.getElementById('button');
 
 let currentDate = new Date();
 
@@ -42,7 +43,6 @@ const updateCalendar = () => {
 
 
 
-
 prevBtn.addEventListener('click', () => {
     currentDate.setMonth(currentDate.getMonth() - 1);
     updateCalendar();
@@ -64,9 +64,20 @@ document.querySelectorAll('.date').forEach(function(day){
 let t = "";
 const timeSelect = ["16:30", "14:00", "18:45", "12:25", "10:40", "16:30", "14:00", "18:45", "12:25", "10:40" ];
 for(let x = 0; x != timeSelect.length; x++){
-t += `<div><input type="checkbox"<lable id="${x}">${timeSelect[x]}<br></div>`;
+t += `<div><input type="radio" name="checkME" onclick="check(${x})" id='checkME${x}' <lable id="${x}">${timeSelect[x]}<br></div>`;
+
 }
 checkclick.innerHTML = t;
+
+function check(a){
+   if(document.getElementById('checkME'+a).checked === true){
+    document.getElementById('btn').style.display = "block";
+   }
+}
+
+   
+   
+
 
 
 document.querySelectorAll('.checkbox').forEach(function(time){
