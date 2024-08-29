fdc5501bfbca680849cb746956bf81d3fe99b886
MrJesperK <you@example.com>
2024-08-29 10:26:58 +0200

    shop filter ruta

--- pages/shop.js
+++ pages/shop.js
@@ -4,3 +4,22 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+
+
+
+function filtersHideShow(){
+    let filters = document.getElementById('filters_hideable');
+let filter_button = document.getElementById('filter_button');
+let label = document.getElementById('filter_label');
+    if (filter_button.checked){
+        label.innerHTML = "Göm filter &uarr;";
+        console.log("open")
+        filters.style.display = "block";
+    } else if (!filter_button.checked){
+        label.innerHTML = "Visa fler filter &darr;";
+        console.log("closed");
+        filters.style.display = "none";
+        label.style.display = "block";
+    }
+}
